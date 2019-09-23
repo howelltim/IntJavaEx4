a98cf0fe0a8230828932ae9756657fd94247c38a
howelltim <th90@ymail.com>
2019-09-23 11:02:09 -0300

    final commit

--- IntJavaEx4/src/intjavaex4/IntJavaEx4.java
+++ IntJavaEx4/src/intjavaex4/IntJavaEx4.java
@@ -1,6 +1,9 @@
 package intjavaex4;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class IntJavaEx4 {
@@ -35,6 +38,16 @@ public class IntJavaEx4 {
         p("Day of Year: " + birthDay.getDayOfYear());
         p("Day of Week: " + birthDay.getDayOfWeek().toString());
         p("Month: " + birthDay.getMonth().toString());
+        
+        Calendar cal = Calendar.getInstance();
+        
+        cal.setTime(Date.valueOf(birthDay));
+        cal.add(Calendar.MONTH, 1);
+        cal.add(Calendar.DATE, 1);
+        p(cal.getTime().toString());
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("yyy/dd/mm");
+        p(sdf.format(cal.getTime()));
     }
     
     public static void p(String msg){
